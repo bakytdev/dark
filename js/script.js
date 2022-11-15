3b384ec55b434ee1f1fe1ecf2ea2905e112d149e
unknown <bakytdev>
2022-11-15 16:18:03 +0600

    end style

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ themeBtn.addEventListener('click', () => {
     themeBtn.style.rotate = '180deg';
     document.body.classList.toggle('dark');
 });
+
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
